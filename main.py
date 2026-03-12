7939ca80c88ed74c9f23d326e577c53d696f8ee1
Lejo33607 <jm8115692@gmail.com>
2026-03-12 06:49:54 -0500

    YAAA

--- main.py
+++ main.py
@@ -1,53 +1,59 @@
-#                                                                                       Students and more...
+#                                                                  Students and more...
 
-Students = [""]
-
-subjects = ["Development", "english", "spanish"]
+students = [] 
+subjects_list = ["Development", "English", "Spanish"]
 
 while True:
-        print("""
-        1. Register student.
-        2. View of students list.
-        3. View
-        4. Get out of here boi.
-        """)
+    print("""
+    1. Register student.
+    2. View students list.
+    3. View grades
+    4. Get out of here boi.
+    """)
 
-        Option = int(input("Enter a option: "))
-        
-        if Option == "1":
-            print("-----------------------------------------")
-            quantity = int(input("How many students do you want to register: "))
-
-            for shi in range(quantity):
-                subjects_and_grade = []
-                Name = input(f"Enter the name of kid #{shi+1}: ")
-                lastname = input(f"Enter the last name of the kid #{shi+1}: ")
-                age = input(f"Enter the age of the kiddo #{shi+1}: ")
-                full_name = Name + " " + lastname
-                for idx, subjects in enumerate():
-                    grade = float(input(f"Enter your grade {subjects}: "))
-                    subjects_and_grade.append({subjects[idx]:grade})
-            Students.append({"Full_name" : full_name,
-                "age" : age,
-                "subjects_and_grade" : subjects_and_grade})
-
-            
-            print(f"Successfully added {quantity} students")
-
-        if Option == "2":
-        
-            print("-------------------------")
-        
-            for shi,idx in range(10):
-                if Students[shi] == "":
-                    print(f"The student in {shi+1} is: {Students[shi]}")
-                else: 
-                    print(f"Students #{shi+1}: ")
+    option = input("Enter an option: ") 
     
-        if Option == "3":
-            print
+    if option == "1":
+        print("-----------------------------------------")
+        quantity = int(input("How many students do you want to register: "))
+
+        for i in range(quantity):
+            subjects_and_grades = []
+            name = input(f"Enter the name of kid #{i+1}: ")
+            lastname = input(f"Enter the last name of the kid #{i+1}: ")
+            age = input(f"Enter the age of the kiddo #{i+1}: ")
+            full_name = f"{name} {lastname}"
+
+            for sub in subjects_list:
+                grade = float(input(f"Enter grade for {sub}: "))
+                subjects_and_grades.append({sub: grade})
+
+            students.append({
+                "Full_name": full_name,
+                "age": age,
+                "grades": subjects_and_grades
+            })
+        
+        print(f"Successfully added {quantity} students")
+
+    elif option == "2":
+        print("--- Student List ---")
+        if not students:
+            print("The list is empty.")
+        for idx, student in enumerate(students):
+            print(f"{idx + 1}. {student['Full_name']} (Age: {student['age']})")
+
+    elif option == "3":
+        print("--- Detailed Grades ---")
+        for student in students:
+            print(f"Student: {student['Full_name']}")
+            for item in student['grades']:
+                for sub, grade in item.items():
+                    print(f"  - {sub}: {grade}")
     
-        else: 
-            Option == "4"
-            print("Ok ma boi")
-            break
+    elif option == "4":
+        print("Ok ma boi, cya!")
+        break
+    
+    else:
+        print("Invalid option, try again.")
